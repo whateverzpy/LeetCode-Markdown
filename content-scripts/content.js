f64c20b2e6b561e02c8e42c8bf2e42839e5d4fa5
ZZZZZpy <2023311810@stu.hit.edu.cn>
2024-05-11 03:07:45 +0800

    功能优化

--- content-scripts/content.js
+++ content-scripts/content.js
@@ -1,21 +1,51 @@
-// content.js
-console.log('内容脚本已加载，正在监听...');
+console.log("内容脚本已加载，正在监听...");
+const turndownService = new TurndownService();
 
-function htmlToMarkdown(html) {
-  const turndownService = new TurndownService();
-  return turndownService.turndown(html);
-}
+turndownService.addRule("betterStars", {
+  // 优化加粗文本的转换
+  filter: ["strong"],
+  replacement: function (content) {
+    return "**" + content + "**" + " ";
+  },
+});
+
+turndownService.addRule("betterExample", {
+  filter: ["pre"],
+  replacement: function (content) {
+    // 分割内容为单独的行
+    var lines = content.split("\n");
+    // 将每行转换为Markdown引用格式
+    var blockquoted = lines
+      .map(function (line, index) {
+        // 检查当前行是否是第一行
+        return (index === 0 ? "\n" : "") + "> " + line;
+      })
+      .join("\n");
+    return blockquoted;
+  },
+});
+
+turndownService.escape = function (text) {
+  // 重写escape方法
+  return text.replace(/([\\`*{}[\]()#+\-.!_>])/g, function (match, char) {
+    // 在这里，我们选择不转义方括号
+    if (char === "[" || char === "]") {
+      return char;
+    }
+    return "\\" + char;
+  });
+};
 
 chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
-  console.log('收到消息:', request);
+  console.log("收到消息:", request);
   if (request.action === "getContent") {
-    const contentElement = document.querySelector('.elfjS');
+    const contentElement = document.querySelector(".elfjS");
     if (contentElement) {
-      const markdown = htmlToMarkdown(contentElement.innerHTML);
-      sendResponse({ content: markdown });
+      const markdownContent = turndownService.turndown(contentElement);
+      sendResponse({ content: markdownContent });
     } else {
-      console.error('找不到指定的元素');
-      sendResponse({ content: '元素未找到' });
+      console.error("找不到指定的元素");
+      sendResponse({ content: "元素未找到" });
     }
     return true; // 保持消息通道开放以异步发送响应
   }
