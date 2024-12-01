de672b726a00d3bedff00533d4f6bdd1edcd40af
longlin10086 <gnulonglin@gmail.com>
2024-12-02 02:36:08 +0800

    feat: add copy button

--- popup/popup.js
+++ popup/popup.js
@@ -11,3 +11,26 @@ document.getElementById("convert").addEventListener("click", function () {
     );
   });
 });
+
+document.addEventListener("DOMContentLoaded", () => {
+  const resultTextarea = document.getElementById("result");
+  const copyButton = document.getElementById("copy");
+
+  copyButton.addEventListener("click", (event) => {
+    event.preventDefault();
+    // 选中文本并复制到剪切板
+    resultTextarea.select();
+    document.execCommand("copy");
+
+    // 修改按钮状态为 ✅
+    copyButton.innerHTML = `
+      <img src="../icons/Check.svg" alt="check" width="18px" />
+    `;
+
+    // 2秒后恢复原始状态
+    setTimeout(() => {
+      copyButton.innerHTML =
+        '<img src="../icons/Copy.svg" alt="copy" width="18px" />';
+    }, 2000);
+  });
+});
